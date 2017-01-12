57f222d3aed3456cb909b4dcaf6d315536627ea6
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-12 17:50:13 +0800

    feat: Added Logs component

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+
+import { AppComponent } from './app.component';
+import { LogsComponent } from './components/logs/logs.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    LogsComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ public/app/components/logs/logs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'logs',
+  templateUrl: 'logs.component.html'
+})
+export class LogsComponent { }
