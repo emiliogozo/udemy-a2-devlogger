7b69cb8da8c6b680723cd38933f430422674b2cd
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-12 17:58:23 +0800

    feat: Added FeathersJS service

--- public/app/components/logs/logs.component.ts
+++ public/app/components/logs/logs.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeathersService } from '../../services/feathers.service';
+import { Log } from '../../Log';
 
 @Component({
   moduleId: module.id,
   selector: 'logs',
   templateUrl: 'logs.component.html'
 })
-export class LogsComponent { }
+export class LogsComponent implements OnInit{
+  logs: Array<Log> = [];
+
+  constructor(private _feathersService: FeathersService) { }
+
+  ngOnInit() {
+    this._feathersService.getLogs().subscribe(logs => {
+      console.log(logs);
+    });
+  }
+}
